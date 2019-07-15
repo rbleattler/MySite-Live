23fc79ba7993291eba2429487abe7c1d8e4327f6
rbleattler <rbleattler@outlook.com>
2019-07-15 10:04:48 -0400

    Adding most recent JS30 projects

--- Test/Startup.cs
+++ Test/Startup.cs
@@ -27,12 +27,13 @@ namespace Test
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
+                options.CheckConsentNeeded = context => false;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
+            services.AddSingleton(typeof(IHttpContextAccessor), typeof(HttpContextAccessor));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddProgressiveWebApp();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
